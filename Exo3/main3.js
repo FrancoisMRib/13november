0bf7249567772b15abe6ca5c0a35525bb130ef70
FracoisMRib <fr.ribere@gmail.com>
2023-11-13 15:27:14 +0100

    3e ajout 3.0

--- Exo3/main3.js
+++ Exo3/main3.js
@@ -10,7 +10,12 @@ switch(true){
        console.log("Le produit est négatif");
         break;
     case (nombre1<0) && (nombre2<0):
+        console.log("Le produit est positif");
         break;
     case (nombre1>0) && (nombre2>0):
+        console.log("Le produit est positif");
+        break;
+    case (nombre1 == 0 || nombre2 == 0):
+        console.log("Le produit est égal à zéro");
         break;
 }
