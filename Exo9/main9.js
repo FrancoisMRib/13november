4ac4becded56024f995f37497809a2dc285188ab
FracoisMRib <fr.ribere@gmail.com>
2023-11-15 10:00:00 +0100

    Correction exo9

--- Exo9/main9.js
+++ Exo9/main9.js
@@ -2,11 +2,14 @@ let quantiteChoco = prompt("Indiquez le nombre de chocolotaines achetées");
 let prixTotal = 0;
 for (let i = 0; i<quantiteChoco ; i++) {
     if (quantiteChoco<=10) {
-        prixTotal = 1.40*quantiteChoco;
+        prixTotal = quantiteChoco*1.4;
+        //prixTotal = 1.40*quantiteChoco;
     } else if (quantiteChoco>10 && quantiteChoco<=20) {
-        prixTotal = 1.40*quantiteChoco + (quantiteChoco-10)*1.30;
+        prixTotal = 10*1.4 + (quantiteChoco-10)*1.3;
+        //prixTotal = 1.40*quantiteChoco + (quantiteChoco-10)*1.30;
     } else if (quantiteChoco>20) {
-        prixTotal = 1.40*quantiteChoco + (quantiteChoco-10)*1.20;
+        prixTotal = 10*1.4 + 10*1.3 + (quantiteChoco-10)*1.2;
+        //prixTotal = 1.40*quantiteChoco + (quantiteChoco-10)*1.30 + (quantiteChoco-20);
     }
 }
 
